48b240b055bf6ec445d2dfc51dd55e374bcf0f9b
caldwmark <caldwmark@gmail.com>
2022-08-26 07:38:15 -0800

    added sticky footer, external links from Link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,41 @@
-import React from "react";
-import { Link } from "gatsby"
+import React from "react"
+
 import { SEO } from "../components/seo"
-import "/static/css/global.css";
+import "/static/css/global.css"
 
 export default function Home() {
   return (
-    <article className='container'>
-      <title>hm servers</title>
-      <div className='main'>
+    <main className="container">
+      <div className="content">
+        <h1>hm servers</h1>
         <p>
-          The hm-servers.com domain is not active serving webpages.  I just keep this for 
-          testing or other unpublished tasks.  Feel free to check back.  But, I wouldn't 
-          expect to find anything here. 
+          The hm-servers.com domain is not active serving webpages. I just keep
+          this for testing or other unpublished tasks. Feel free to check back.
+          But, I wouldn't expect to find anything here.
         </p>
         <div>
           Visit some of my more active projects
-          <ul className='links'>
-            <li><Link to="https://www.hillbillyradio.net">Hillbilly Radio</Link></li>
-            <li><Link to='https://www.bearcountrypublishing.com'>Bear Country Publishing</Link></li>
+          <ul className="links">
+            <li>
+              <a href="https://www.hillbillyradio.net">Hillbilly Radio</a>
+            </li>
+            <li>
+              <a href="https://bearcountrypublishing.com">
+                Bear Country Publishing
+              </a>
+            </li>
           </ul>
         </div>
       </div>
-    </article>
+      <footer>
+        This page is available on
+        <a href="https://github.com/caldwmark/gatsby-simple-splash-page">
+          {" "}
+          Github
+        </a>
+      </footer>
+    </main>
   )
 }
 
-export const Head = () => (
-  <SEO />
-)
+export const Head = () => <SEO />
